32dfc8a47a422da8222b68411fe5df7d8e80a728
DESKTOP-7QQ3K3B\Moni <monica_97_ct@hotmail.com>
2025-03-18 10:35:48 +0100

    first commit

---
+++ contador/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Contador from './components/Contador.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1>Contador</h1>
+    <Contador />
+  </div>
+</template>
+
+
---
+++ contador/src/components/Contador.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container_contador">
+        <div id="contador">
+            <button @click="decrementar" :disabled="disabled">-</button>
+            <p >{{ contador }}</p>
+            <button @click="incrementar">+</button>
+        </div>
+        <div id="resetear" :style="opacidad" >
+            <button @click="resetear">Resetear</button>
+        </div>
+    </div>
+    
+</template>
+
+<script setup>
+    import { ref, computed } from 'vue';
+    const contador = ref(0);
+    const incrementar = () => {
+        contador.value++;
+    }
+    const decrementar = () => {
+        contador.value--;
+    }
+    const resetear = () => {
+        contador.value = 0;
+    }
+    const opacidad = computed(() => ({opacity: contador.value > 0 ? 1 : 0.5})); /*opacidad*/
+    const disabled = computed(() => contador.value <= 0); /*desabilitar boton*/
+</script>
+
